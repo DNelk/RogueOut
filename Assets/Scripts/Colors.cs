cc313028b9615581c7c3c9e10627100df74a101b
Dylan Nelkin <dylannelkin@gmail.com>
2021-09-17 22:47:52 -0600

    Unity Project Add

---
+++ Assets/Scripts/Colors.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Colors
+{
+    public static Color Yellow = new Color(0.992f, 0.891f, 0.25f);
+    public static Color Purple = new Color(0.605f, 0.363f, 0.895f);
+    public static Color Pink = new Color(0.254f, 0.228f, 0.64f);
+    public static Color Blue = new Color(0.254f, 0.228f, 0.64f);
+    public static Color Teal = new Color(0.254f, 0.228f, 0.64f);
+}
